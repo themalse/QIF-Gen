34945216c734ef3e9602a5482bdc402a0a68ef92
themalse <60769060+themalse@users.noreply.github.com>
2020-06-27 18:00:43 +1200

    Tidy up code, switching most vars to arrays

--- QIF-Gen.ps1
+++ QIF-Gen.ps1
@@ -1,49 +1,29 @@
 $revision = 'v0.7'
-$QIFFileName = 'test.QIF' #File name
 
 # Clear variables if used Globally
 $StartDate = ''
 $EndDate = ''
 $TransactionsPerDay = ''
 $TotalDays = ''
+$QIFFileName = 'test.QIF' #File name to use as default
 
-# UI COLOUR FORMATTING #
-$IsDefaultFG = (Get-Host).UI.RawUI.ForegroundColor
-$IsDefaultBG = 'black'
-$IsInvalidFG = 'red'
-$IsInvalidBG = $IsDefaultBG
-$lblInFocusFG = 'cyan'
-$lblInFocusBG = $IsDefaultBG
-#$InFocusFG = 'cyan'
-#$InFocusBG = $IsDefaultBG
-$lblIsSetFG = 'green'
-$lblIsSetBG = $IsDefaultBG
-$IsSetFG = 'green'
-$IsSetBG = $IsDefaultBG
+# UI colour formatting | [Forground Label,Background Label,Foreground Val,Background Val]
+$DefaultCol = 'gray','black','gray','black'
+$ValidCol = 'green',$DefaultCol[1],'green',$DefaultCol[1]
+$InvalidCol = 'red',$DefaultCol[1],'red',$DefaultCol[1]
+$InFocusCol = 'cyan',$DefaultCol[1],'cyan',$DefaultCol[1]
 
-# Set UI background colour to black
-$Host.UI.RawUI.BackgroundColor = 'black'
+# Set default UI colours
+$Host.UI.RawUI.ForegroundColor = $DefaultCol[0]
+$Host.UI.RawUI.BackgroundColor = $DefaultCol[1]
+$StartDateCol = $DefaultCol[0],$DefaultCol[1],$DefaultCol[0],$DefaultCol[1]
+$EndDateCol = $DefaultCol[0],$DefaultCol[1],$DefaultCol[0],$DefaultCol[1]
+$TransactionsCol = $DefaultCol[0],$DefaultCol[1],$DefaultCol[0],$DefaultCol[1]
 
-$lblStartDateFG = $IsDefaultFG
-$lblStartDateBG = $IsDefaultFG
-$StartDateFG = $IsDefaultFG
-$StartDateBG = $IsDefaultBG
-
-$lblEndDateFG = $IsDefaultFG
-$lblEndDateBG = $IsDefaultFG
-$EndDateFG = $IsDefaultFG
-$EndDateBG = $IsDefaultBG
-
-$lblTransactionsFG = $IsDefaultFG
-$lblTransactionsBG = $IsDefaultFG
-$TransactionsFG = $IsDefaultFG
-$TransactionsBG = $IsDefaultBG
-
-# VALIDITY CHECKS #
-$StartDateValid = $false
-$EndDateValid = $false
-$TPerDayValid = $false
-$IsConfirmed = $false
+# Validity Checks
+$StartDateValid = $null,$false
+$EndDateValid = $null,$false
+$TransactionsValid = $false
 $AllValid = $false
 
 function Test-Date($String) {
@@ -51,7 +31,7 @@ function Test-Date($String) {
         return $true
     }   catch 
         {
-            return $false
+            return $null,$false
         }
 }
 
@@ -68,94 +48,47 @@ function Test-Int ([string]$x) {
 
 function Update-UI {
     if ($AllValid -eq $true) {
-        $lblStartDateFG = $lblIsSetFG
-        $lblStartDateBG = $lblIsSetBG
-        $StartDateFG = $IsSetFG
-        $StartDateBG = $IsSetBG
-        $lblEndDateFG = $lblIsSetFG
-        $lblEndDateBG = $lblIsSetBG
-        $EndDateFG = $IsSetFG
-        $EndDateBG = $IsSetBG
-        $lblTransactionsFG = $lblIsSetFG
-        $lblTransactionsBG = $lblIsSetBG
-        $TransactionsFG = $IsSetFG
-        $TransactionsBG = $IsSetBG
-    }   elseif ($StartDateValid -eq $false) {
-            $lblStartDateFG = $lblInFocusFG
-            $lblStartDateBG = $lblInFocusBG
-            #$StartDateFG = $InFocusFG
-            #$StartDateBG = $InFocusBG
-            $lblEndDateFG = $IsDefaultFG
-            $lblEndDateBG = $IsDefaultBG
-            $EndDateFG = $IsDefaultFG
-            $EndDateBG = $IsDefaultBG
-            $lblTransactionsFG = $IsDefaultFG
-            $lblTransactionsBG = $IsDefaultBG
-            $TransactionsFG = $IsDefaultFG
-            $TransactionsBG = $IsDefaultBG
-        }   elseif ($EndDateValid -eq $false) {
-                $lblStartDateFG = $lblIsSetFG
-                $lblStartDateBG = $lblIsSetBG
-                $StartDateFG = $IsSetFG
-                $StartDateBG = $IsSetBG
-                $lblEndDateFG = $lblInFocusFG
-                $lblEndDateBG = $lblInFocusBG
-                #$EndDateFG = $InFocusFG
-                #$EndDateBG = $InFocusBG
-                $lblTransactionsFG = $IsDefaultFG
-                $lblTransactionsBG = $IsDefaultBG
-                $TransactionsFG = $IsDefaultFG
-                $TransactionsBG = $IsDefaultBG
-            }   elseif ($TPerDayValid -eq $false) {
-                    $lblStartDateFG = $lblIsSetFG
-                    $lblStartDateBG = $lblIsSetBG
-                    $StartDateFG = $IsSetFG
-                    $StartDateBG = $IsSetBG
-                    $lblEndDateFG = $lblIsSetFG
-                    $lblEndDateBG = $lblIsSetBG
-                    $EndDateFG = $IsSetFG
-                    $EndDateBG = $IsSetBG
-                    $lblTransactionsFG = $lblInFocusFG
-                    $lblTransactionsBG = $lblInFocusBG
-                    #$TransactionsFG = $InFocusFG
-                    #$TransactionsBG = $InFocusBG
-                }   elseif ($TPerDayValid -eq $true) {
-                        $lblStartDateFG = $lblIsSetFG
-                        $lblStartDateBG = $lblIsSetBG
-                        $StartDateFG = $IsSetFG
-                        $StartDateBG = $IsSetBG
-                        $lblEndDateFG = $lblIsSetFG
-                        $lblEndDateBG = $lblIsSetBG
-                        $EndDateFG = $IsSetFG
-                        $EndDateBG = $IsSetBG
-                        $lblTransactionsFG = $IsSetFG
-                        $lblTransactionsBG = $IsSetBG
-                        $TransactionsFG = $IsSetFG
-                        $TransactionsBG = $IsSetBG
+        $StartDateCol = $ValidCol
+        $EndDateCol = $ValidCol
+        $TransactionsCol = $ValidCol
+    }   elseif ($StartDateValid[1] -eq $false) {
+            $StartDateCol = $InFocusCol
+            $EndDateCol = $DefaultCol
+            $TransactionsCol = $DefaultCol
+        }   elseif ($EndDateValid[1] -eq $false) {
+                $StartDateCol = $ValidCol
+                $EndDateCol = $InFocusCol
+                $TransactionsCol = $DefaultCol
+            }   elseif ($TransactionsValid -eq $false) {
+                    $StartDateCol = $ValidCol
+                    $EndDateCol = $ValidCol
+                    $TransactionsCol = $InFocusCol
+                }   elseif ($TransactionsValid -eq $true) {
+                    $StartDateCol = $ValidCol
+                    $EndDateCol = $ValidCol
+                    $TransactionsCol = $ValidCol
                     }
-
-    Write-Host "QIF Dummy Data Builder $revision"
+    Clear-Host
+    Write-Host 'QIF-Gen'$revision': Builds a QIF file with dummy data'
     Write-Host
     Write-Host "Date Ranges to use for Dummy QIF file"
-    Write-Host "Start Date: " -NoNewline -ForegroundColor $lblStartDateFG -BackgroundColor $lblStartDateBG
-    try { $StartDate.ToString("dd/MM/yyyy") | Out-Null
-        Write-Host $StartDate.ToString("dd/MM/yyyy") -ForegroundColor $StartDateFG -BackgroundColor $StartDateBG
-    }
-    catch {
-        Write-Host $StartDate -ForegroundColor $StartDateFG -BackgroundColor $StartDateBG
-    }
-    Write-Host "End Date: " -NoNewline -ForegroundColor $lblEndDateFG -BackgroundColor $lblEndDateBG
-    try { $EndDate.ToString("dd/MM/yyyy") | Out-Null
-        Write-Host $EndDate.ToString("dd/MM/yyyy") -ForegroundColor $EndDateFG -BackgroundColor $EndDateBG
-    }
-    catch {
-        Write-Host $EndDate -ForegroundColor $EndDateFG -BackgroundColor $EndDateBG
-    }
+    Write-Host "Start Date: " -NoNewline -ForegroundColor $StartDateCol[0] -BackgroundColor $StartDateCol[1]
+    if ($StartDateValid[1] -eq $false) {
+        Write-Host $StartDate -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+    }   else {
+        Write-Host $StartDate.ToString("dd/MM/yyyy") -ForegroundColor $ValidCol[0] -BackgroundColor $ValidCol[1]
+        }
+    Write-Host "End Date: " -NoNewline -ForegroundColor $EndDateCol[0] -BackgroundColor $EndDateCol[1]
+    if ($EndDateValid[1] -eq $false) {
+        Write-Host $EndDate -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+    }   else {
+            Write-Host $EndDate.ToString("dd/MM/yyyy") -ForegroundColor $ValidCol[0] -BackgroundColor $ValidCol[1]
+        }
     Write-Host
-    Write-Host "Transactions/Day: " -NoNewline -ForegroundColor $lblTransactionsFG -BackgroundColor $lblTransactionsBG
-    Write-Host $TransactionsPerDay -ForegroundColor $TransactionsFG -BackgroundColor $TransactionsBG
+    Write-Host "Transactions/Day: " -NoNewline -ForegroundColor $TransactionsCol[0] -BackgroundColor $TransactionsCol[1]
+    Write-Host $TransactionsPerDay -ForegroundColor $TransactionsCol[2] -BackgroundColor $TransactionsCol[3]
     Write-Host
-    Write-Host "Total number of days to record: $TotalDays"
+    Write-Host "Total number of days: $TotalDays"
     try { Test-Int $TotalDays | Out-Null
         Write-Host 'Maxmimum possible number of transactions to generate:' ($TotalDays * $TransactionsPerDay) #ADDCOLOR
     }
@@ -165,134 +98,114 @@ function Update-UI {
     Write-Host
 }
 
-Clear-Host #Required if script run as a command
-Write-Host #Blank 
+Clear-Host #Clears terminal
+Update-UI
 
 while ($AllValid -eq $false) {
-    Update-UI
-    if ($StartDateValid -eq $false) {
-        $StartDate = Read-Host -Prompt 'Enter starting date (dd/MM/yyyy) for QIF dummy data' 
-        Clear-Host
+    if ($StartDateValid[1] -eq $false) {
+        $StartDate = Read-Host -Prompt 'Enter starting date (dd/MM/yyyy)' 
         if ($StartDate -eq '') {
-            Write-Host 'Start Date' -NoNewline -ForegroundColor $IsInvalidFG -BackgroundColor $IsInvalidBG
-            Write-Host " cannot be empty" -ForegroundColor $IsDefaultFG -BackgroundColor $IsDefaultBG
-            $StartDateFG = $IsInvalidFG
-            $StartDateBG = $IsInvalidBG
-            }   else {
-                    $StartDateValid = Test-Date $StartDate
-                    if ($StartDateValid -eq $false) {
-                        Write-Host $StartDate -NoNewline -ForegroundColor $IsInvalidFG -BackgroundColor $IsInvalidBG
-                        Write-Host " is not a valid date format." -ForegroundColor $IsDefaultFG -BackgroundColor $IsDefaultBG
-                        $StartDateFG = $IsInvalidFG
-                        $StartDateBG = $IsInvalidBG
-                    }   else {
-                            $StartDate = $StartDateValid[0]
-                            Write-Host
-                        }
-                }
+            Update-UI
+            Write-Host 'Start Date' -NoNewline -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+            Write-Host " cannot be empty" -ForegroundColor $DefaultCol[0] -BackgroundColor $DefaultCol[1]
+        }   else {
+                $StartDateValid = Test-Date $StartDate
+                if ($StartDateValid[1] -eq $false) {
+                    Update-UI
+                    Write-Host $StartDate -NoNewline -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+                    Write-Host " is not a valid date format." -ForegroundColor $DefaultCol[0] -BackgroundColor $DefaultCol[1]
+                }   else {
+                        $StartDate = $StartDateValid[0]
+                        Update-UI
+                    }
+            }
     }
 
-    elseif ($EndDateValid -eq $false) {
-        $EndDate = Read-Host -Prompt 'Enter ending date (dd/MM/yyyy) for QIF dummy data'
-        Clear-Host
+    elseif ($EndDateValid[1] -eq $false) {
+        $EndDate = Read-Host -Prompt 'Enter ending date (dd/MM/yyyy)'
         if ($EndDate -eq '') {
-            Write-Host "End Date" -NoNewline -ForegroundColor $IsInvalidFG -BackgroundColor $IsInvalidBG
-            Write-Host " cannot be empty" -ForegroundColor $IsDefaultFG -BackgroundColor $IsDefaultBG
-            $EndDateFG = $IsInvalidFG
-            $EndDateBG = $IsInvalidBG
+            Update-UI
+            Write-Host 'End Date' -NoNewline -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+            Write-Host ' cannot be empty' -ForegroundColor $DefaultCol[0] -BackgroundColor $DefaultCol[1]
         }   else {
                 $EndDateValid = Test-Date $EndDate
-                if ($EndDateValid -eq $false) {
-                    Write-Host $EndDate -NoNewline -ForegroundColor $IsInvalidFG -BackgroundColor $IsInvalidBG
-                    Write-Host " is not a valid date format." -ForegroundColor $IsDefaultFG -BackgroundColor $IsDefaultBG
-                    $EndDateFG = $IsInvalidFG
-                    $EndDateBG = $IsInvalidBG
+                if ($EndDateValid[1] -eq $false) {
+                    Update-UI
+                    Write-Host $EndDate -NoNewline -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+                    Write-Host ' is not a valid date format.' -ForegroundColor $DefaultCol[0] -BackgroundColor $DefaultCol[1]
                 }   else {
                         $EndDate = $EndDateValid[0]
                         if ($StartDate -gt $EndDate) {
-                            $EndDateValid = $false
-                            Write-Host "End Date" -NoNewline -ForegroundColor $IsInvalidFG -BackgroundColor $IsInvalidBG
-                            Write-Host " cannot be before the Start Date" -ForegroundColor $IsDefaultFG -BackgroundColor $IsDefaultBG
-                            $EndDateFG = $IsInvalidFG
-                            $EndDateBG = $IsInvalidBG
+                            $EndDateValid[1] = $false
+                            Write-Host 'End Date' -NoNewline -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+                            Write-Host ' cannot be before the Start Date' -ForegroundColor $DefaultCol[0] -BackgroundColor $DefaultCol[1]
                         }   else {
                                 $EndDate = $EndDateValid[0]
-                                Clear-Host
-                                Write-Host
+                                Update-UI
                             }
                     }
             }
     }
 
-    elseif ($TPerDayValid -eq $false) {
-        $TransactionsPerDay = Read-Host "Enter the maximum possible number of transactions to generate per day"
-        Clear-Host
+    elseif ($TransactionsValid -eq $false) {
+        $TransactionsPerDay = Read-Host 'Enter maximum random transactions per day'
         if ($TransactionsPerDay -eq '') {
-            Write-Host "Transactions/Day" -NoNewline -ForegroundColor $IsInvalidFG -BackgroundColor $IsInvalidBG
-            Write-Host " cannot be empty" -ForegroundColor $IsDefaultFG -BackgroundColor $IsDefaultBG
+            Update-UI
+            Write-Host "Transactions/Day" -NoNewline -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+            Write-Host " cannot be empty" -ForegroundColor $DefaultCol[0] -BackgroundColor $DefaultCol[1]
         }   else {
-                $TPerDayValid = Test-Int $TransactionsPerDay
-                if ($TPerDayValid -eq $false) {
-                    $TransactionsFG = $IsInvalidFG
-                    $TransactionsBG = $IsInvalidBG
-                    Write-Host $TransactionsPerDay -NoNewline -ForegroundColor $IsInvalidFG -BackgroundColor $IsInvalidBG
-                    Write-Host " must be an integer greater than or equal to 1" -ForegroundColor $IsDefaultFG -BackgroundColor $IsDefaultBG
+                $TransactionsValid = Test-Int $TransactionsPerDay
+                if ($TransactionsValid -eq $false) {
+                    Update-UI
+                    Write-Host $TransactionsPerDay -NoNewline -ForegroundColor $InvalidCol[0] -BackgroundColor $InvalidCol[1]
+                    Write-Host " must be an integer greater than or equal to 1" -ForegroundColor $DefaultCol[0] -BackgroundColor $DefaultCol[1]
                 }   else {
-                        $TransactionsFG = $IsSetFG
-                        $TransactionsBG = $IsSetBG
-                        $ts = New-TimeSpan -Start $StartDate -End $EndDate
-                        $TotalDays = $ts.Days + 1
-                        Clear-Host
-                        Write-Host
+                        $TotalDays = (New-TimeSpan -Start $StartDate -End $EndDate).Days + 1
                         Update-UI
-                        $msg = 'Is the Transactions/Day value correct? [Y/N]' #ADDCOLOR
+                        $msg = 'Are Transactions/Day value correct? [Y|N]' #ADDCOLOR
                         do {
                             $response = Read-Host -Prompt $msg
                             if ($response -eq 'n') {
-                                $TPerDayValid = $false
+                                $TransactionsValid = $false
                                 $response = 'y'
                             }
                         } until ($response -eq 'y')
-                        Clear-Host
-                        Write-Host
-
+                        Update-UI
                     }
             }
     }
-    
-    # TODO: Show actual number of transactions generated
-    elseif ($IsConfirmed -eq $false) {
-        $msg = 'Ready to create the QIF? [Y = Proceed | N = Quit]'
+
+    elseif (($StartDateValid -eq $true) -and ($EndDateValid -eq $true) -and ($TransactionsValid -eq $true)) {
         do {
-            $response = Read-Host -Prompt $msg
+            $response = Read-Host -Prompt 'Create QIF? [Y|N]'
             if ($response -eq 'y') {
-                $QIFContent = for ($i = 0; $i -lt $TotalDays; $i++) {
-                    $jcount = Get-Random -Minimum 1 -Maximum $TransactionsPerDay
-                    for ($j = 0; $j -lt $jcount; $j++) {
-                        $tempnum = 0
-                        $tempi = $StartDate.AddDays($i).ToString('dd/MM/yyyy')
-                        "D$tempi"
-                        while ($tempnum -eq 0) {
-                            $tempnum = [math]::Round((Get-Random -Minimum -100000.00 -Maximum 100000.00),2)
-                        }
-                        "T$tempnum"
-                        if ($tempnum -lt 0) {
-                            "PWithdrawal"
-                        }   else {
-                                "PDeposit"
-                        }
-                        "M"
-                        "^"
-                    }
-                }
+                $QIFContent =   for ($i = 0; $i -lt $TotalDays; $i++) {
+                                    $jcount = Get-Random -Minimum 1 -Maximum $TransactionsPerDay
+                                    for ($j = 0; $j -lt $jcount; $j++) {
+                                        $tempi = $StartDate.AddDays($i).ToString('dd/MM/yyyy')
+                                        "D$tempi"
+                                        $tempnum = 0 #Avoid creating $0.00 transaction
+                                        while ($tempnum -eq 0) {
+                                            $tempnum = [math]::Round((Get-Random -Minimum -100000.00 -Maximum 100000.00),2)
+                                        }
+                                        "T$tempnum"
+                                        if ($tempnum -lt 0) { #Determine if transaction is Withdrawal or Deposit
+                                            "PWithdrawal"
+                                        }   else {
+                                                "PDeposit"
+                                            }
+                                        "M"
+                                        "^"
+                                    }
+                                }
                 Out-File .\$QIFFileName -Encoding ascii
                 Add-Content $QIFFileName '!Type:Bank'
                 Add-Content $QIFFileName $QIFContent
             }
-            Write-Host "The file has been saved to:"(Get-Location).Path"\"$QIFFileName
+            Write-Host $QIFFileName 'is saved to:'(Get-Location).Path
             $response = 'n'
         } until ($response -eq 'n')
+        Read-Host -Prompt "Press Enter exit the tool"
         $AllValid = $true
     }
 }
-Read-Host -Prompt "Press Enter exit the tool"
